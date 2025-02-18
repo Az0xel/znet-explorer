a4929e18e16215497969139f8964a3eb4daf8ce6
Az0xel <t.voltari@gmail.com>
2025-02-18 09:12:27 +0100

    Fix: Update build configuration and lockfile

--- znet-explorer/next.config.ts
+++ znet-explorer/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const config = {
+  output: 'export',
+  distDir: 'dist',
+  images: { unoptimized: true }
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default config
